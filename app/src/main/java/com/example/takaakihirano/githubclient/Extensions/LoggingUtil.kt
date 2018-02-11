6ce350a8737e4c08d635c99bb3d666da98be3975
hiramekun <thescript1210@gmail.com>
2018-02-11 20:06:56 +0900

    Implement Logging util

---
+++ app/src/main/java/com/example/takaakihirano/githubclient/Extensions/LoggingUtil.kt
@@ -0,0 +1,26 @@
+package com.example.takaakihirano.githubclient.Extensions
+
+import android.util.Log
+import com.example.takaakihirano.githubclient.BuildConfig
+
+/**
+ * Created by takaakihirano on 2018/02/11.
+ */
+
+fun String.log_d(tag: String = "") {
+    if (BuildConfig.DEBUG) {
+        Log.d(tag, this)
+    }
+}
+
+fun Int.log_d(tag: String = "") {
+    if (BuildConfig.DEBUG) {
+        Log.d(tag, toString())
+    }
+}
+
+fun errorIfDebug() {
+    if (BuildConfig.DEBUG) {
+        error("")
+    }
+}
