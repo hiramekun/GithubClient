f00542bc205669ba0643db5840c2ffc3dbf3e515
hiramekun <thescript1210@gmail.com>
2018-02-13 19:24:53 +0900

    Reformat file

--- app/src/main/java/com/example/takaakihirano/githubclient/data/entity/UserEntity.kt
+++ app/src/main/java/com/example/takaakihirano/githubclient/data/entity/UserEntity.kt
@@ -11,9 +11,9 @@ import io.realm.annotations.PrimaryKey
 @JsonIgnoreProperties(ignoreUnknown = true)
 open class UserEntity(
         @PrimaryKey
-        open var id: Int=0,
-        open var login: String="",
-        open var avatar_url: String="",
-        open var name: String="",
-        open var url: String=""
+        open var id: Int = 0,
+        open var login: String = "",
+        open var avatar_url: String = "",
+        open var name: String = "",
+        open var url: String = ""
 ) : RealmObject()
