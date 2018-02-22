7f8a8952ed29a04cf4936b287f1ddd2d4809910b
hiramekun <thescript1210@gmail.com>
2018-02-22 22:27:05 +0900

    Remove SharedPreference repo

--- app/src/main/java/com/example/takaakihirano/githubclient/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/takaakihirano/githubclient/presentation/view/MainActivity.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.takaakihirano.githubclient.R
-import com.example.takaakihirano.githubclient.data.repository.SharedPreferenceRepo
+import com.example.takaakihirano.githubclient.data.repository.AuthInfoRepo
 import com.example.takaakihirano.githubclient.presentation.navigation.Navigator
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (SharedPreferenceRepo.getAccessToken(this).isEmpty()) {
+        if (AuthInfoRepo.getAccessToken().isEmpty()) {
             Navigator.navigateToLogin(this)
             finish()
         }
